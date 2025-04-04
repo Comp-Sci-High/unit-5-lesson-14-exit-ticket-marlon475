7b2b0ab5521a11be6edd2a452206cb2e4ba58d6a
marlon475 <marlon.cruz-estevez25@compscihigh.org>
2025-04-04 19:12:55 +0000

    Complete

--- public/script.js
+++ public/script.js
@@ -4,4 +4,27 @@
 
 // Call the function [1 pt]
 // Show us the successful POST fetch [1 pt]
+async function createPotion() {
+    try {
+        // fill in the path
+        const response = await fetch(base + "/create-potion", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                label: "Flying Potion",
+                effect: "Allows you to fly",
+                ingredients: "Swan Feathers, Eagle Tongue, Hawk Claws",
+                color: "Baby Blue",
+                isExplosive: false
+            })
 
+        });
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+createPotion()
